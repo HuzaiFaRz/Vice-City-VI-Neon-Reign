3554831b97002d64aa5e44eebd7c5670e517ebe9
HuzaifaRz <huziafarazaa999@gmail.com>
2025-06-01 02:16:26 +0500

    Loading Animation Complete

--- src/Components/VI_Svg.jsx
+++ src/Components/VI_Svg.jsx
@@ -1,23 +1,48 @@
-import React, { Fragment, use } from "react";
+import React, { Fragment } from "react";
 import VIBackground from "../assets/Images/Background.png";
 
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useState } from "react";
 const VI_Svg = () => {
+  // const [first, setfirst] = useState(true)
   useGSAP(() => {
     const gsapTimeLine = gsap.timeline();
-    gsapTimeLine.to("vi-mask-group", {
-      rotate: 23,
-    });
+    gsapTimeLine
+      .to(".VI-mask-group", {
+        rotate: 10,
+        duration: 2,
+        ease: "power4.easeInOut",
+        transformOrigin: "50% 50%",
+      })
+      .to(".VI-mask-group", {
+        scale: 10,
+        duration: 2,
+        delay: -1.8,
+        ease: "Expo.easeInOut",
+        transformOrigin: "50% 50%",
+        opacity: 0,
+        onUpdate: function () {
+          if (this.progress() >= 0.9) {
+            document.querySelector(".VI_svg").remove();
+            this.kill();
+            return;
+          }
+        },
+      });
   });
 
   return (
     <Fragment>
-      <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+      <svg
+        viewBox="0 0 800 600"
+        preserveAspectRatio="xMidYMid slice"
+        className="VI_svg"
+      >
         <defs>
           <mask id="VIMask">
             <rect className="w-full h-full fill-black" />
-            <g className="vi-mask-group">
+            <g className="VI-mask-group">
               <text
                 className="translate-x-3/6 translate-y-3/6 text-[250px] fill-white"
                 textAnchor="middle"
